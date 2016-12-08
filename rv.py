8cb91e19fdebd9a365694a783b45c81a9ce9671e
Jason Neal <jason.neal@astro.up.pt>
2016-12-08 15:08:04 +0000

    Initalize script. Load parameter file into a dictionary.

---
+++ rv.py
@@ -0,0 +1,50 @@
+
+# Radial Velocity calculations:
+
+# Goals:
+# To calcualte when the radial velocity is different by a certian value.
+# Plot radial velocity phase curves. Indicating obtained measurement locations
+
+import argparse
+
+
+def _parser():
+    parser = argparse.ArgumentParser(description='Radial velocity plotting')
+    parser.add_argument('params', help='RV parameters filename')
+    # parser.add_argument('-d', '--date', default='today',
+    #                     help='Date in format YYYY-MM-DD. Default is today.')
+    # #  prediciting separated lines
+    # parser.add_argument('-r', '--rv_diff', help='RV difference threshold to find')
+    # parser.add_argument('-o', '--obs_times',  help='Times of previous observations', nargs='+')
+    # parser.add_argument('-f', '--files', help='Params and obs-times are file'
+    #                    ' names to open', action='store_true')
+    # parser.add_argument('-m', '--mode', help='Display mode '
+    #                     ' e.g. phase or time plot. Default="phase"',
+    #                     choices=['time', 'phase'], default='phase')
+    return parser.parse_args()
+
+
+def main(params):  # obs_times=None, mode='phase', rv_diff=None
+    """ Do main stuff """
+
+    # Load in params and store as a dictionary
+    parameters = dict()
+    with open(params, 'r') as f:
+        for line in f:
+            if line.startswith("#"):
+                pass
+            else:
+                par, val = line.lower().split('=')
+                parameters[par.strip(' ')] = val.strip(' ')
+            print(line)
+
+    print(parameters)
+
+
+if __name__ == '__main__':
+
+    args = vars(_parser())
+    # star_name = args.pop('star_name')
+    opts = {k: args[k] for k in args}
+
+    main(**opts)
