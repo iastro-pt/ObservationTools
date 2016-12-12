0e5b1f0991fc6173dc9f821392ba9c41cf67d79d
Jason Neal <jason.neal@astro.up.pt>
2016-12-12 22:36:39 +0000

    Calculate RV amplitude of Companion using the mass ratio.

--- rv.py
+++ rv.py
@@ -24,6 +24,29 @@ def _parser():
     return parser.parse_args()
 
 
+def companion_semimajor(k_host, m_host, m_companion):
+    """ Calcualte the companion RV semi-major axis
+
+    Parameters
+    ----------
+    k_host: float
+        Semi-major amplitude of Radial velocity variation of host.
+    m_host: float
+        Mass of host
+    m_companion: float
+        Mass of companion in consistent units.
+
+    Returns
+    -------
+    k_companion: float
+        Semi-major RV amplitude of companion.
+
+    """
+    sun_jupiter_mass = 1047.56  # Solar mass in jupiter masses
+    m_host *= sun_jupiter_mass  # Convert to jupiter mass
+    return - k_host * m_host / m_companion
+
+
 def main(params):  # obs_times=None, mode='phase', rv_diff=None
     """ Do main stuff """
 
@@ -40,9 +63,18 @@ def main(params):  # obs_times=None, mode='phase', rv_diff=None
     # Turn most parameters to floats.
     for key in parameters.keys():
         if key in ['mean_val', 'k1', 'omega', 'eccentricity', 'tau', 'Period',
-                     'm_star', 'msini ', 'm_true']:
+                   'm_star', 'msini ', 'm_true']:
             parameters[key] = float(parameters[key])
 
+    # Calculate companion semi-major axis
+    if 'k2' in parameters.keys():
+        pass
+    else:
+        if 'm_true' in parameters.keys():
+            # Use true mass if given
+            parameters['k2'] = companion_semimajor(parameters['k1'], parameters['m_star'], parameters['m_true'])
+        else:
+            parameters['k2'] = companion_semimajor(parameters['k1'], parameters['m_star'], parameters['msini'])
 
     print(parameters)
 
