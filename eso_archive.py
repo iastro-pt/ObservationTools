a70dca4d7fc38440d94f751259154d3d7dcf5da8
DanielAndreasen <daniel.andreasen@astro.up.pt>
2017-05-09 17:33:02 +0100

    Add start for ESO archive

---
+++ eso_archive.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+# My imports
+from __future__ import division
+import numpy as np
+from astroquery.eso import Eso
+import argparse
+
+
+def _parser():
+    eso = Eso()
+    instruments = eso.list_instruments()
+    parser = argparse.ArgumentParser(description='Search and download from the ESO archive.')
+    parser.add_argument('username', help='ESO username')
+    parser.add_argument('--store_password', help='Store ESO password', default=False, action='store_true')
+    parser.add_argument('--object', help='Object, e.g. "Aldebaran"', default=False)
+    parser.add_argument('--instrument', help='ESO instruments', choices=instruments, type=str.lower)
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = _parser()
+    # Login
+    eso = Eso()
+    eso.login(args.username, store_password=args.store_password)
+
+    target = args.object
+    instrument = args.instrument
+    table = eso.query_instrument(instrument, column_filters={'target': target})
+    table.pprint()
+
+    # data_files = eso.retrieve_data(table['DP.ID'])
+    # print data_files
