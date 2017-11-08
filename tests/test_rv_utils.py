95c7f13ac287f13cd85072c25dd33cdac9f34494
Jason Neal <jason.neal@astro.up.pt>
2017-11-08 13:24:14 +0000

    Enable deadline setting.

--- tests/test_rv_utils.py
+++ tests/test_rv_utils.py
@@ -51,7 +51,7 @@ def test_RV_from_params_circular(params):
 
 
 # mean_val k1 period tau omega eccentricity
-#@settings(deadline=400)  # double deadline for this test
+@settings(deadline=400)  # double deadline for this test
 @given(st.floats(min_value=0, max_value=1e6, allow_nan=False, allow_infinity=False),
        st.floats(min_value=0.1, max_value=1e5, allow_nan=False, allow_infinity=False),
        st.floats(min_value=0.1, max_value=1e6, allow_nan=False, allow_infinity=False),
